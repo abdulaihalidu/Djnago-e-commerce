b0791d167b6bcaa61b73f404ec823e70efc57916
Haliduabdulai <abdulaihalidu008@gmail.com>
2021-09-03 11:47:26 +0300

    fourth commit

--- BuyAfriq/settings.py
+++ BuyAfriq/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z54rmrb%z_ueb7unkb&984#i45u)744a9b+td6j#rec(6*h1&z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://buyafriq.herokuapp.com', 'localhost', ]
 
 
 # Application definition
